9993e5b7e0bedbe32b72960c08fb65de53a42969
Yo Kobayashi <82624334+KobayashiYoh@users.noreply.github.com>
2024-10-23 01:01:28 +0900

    LaunchPageに画面遷移の処理を追加

--- mobile/lib/pages/LaunchPage.dart
+++ mobile/lib/pages/LaunchPage.dart
@@ -1,10 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:mobile/repositories/SecureStorageRepository.dart';
 
-class LaunchPage extends StatelessWidget {
+class LaunchPage extends StatefulWidget {
   const LaunchPage({super.key});
 
   @override
+  State<LaunchPage> createState() => _LaunchPageState();
+}
+
+class _LaunchPageState extends State<LaunchPage> {
+  @override
+  void initState() {
+    super.initState();
+    Future(() async {
+      final token = await SecureStorageRepository().readToken();
+      if (!mounted) return;
+      if (token == null) {
+        context.go('/top');
+        return;
+      }
+      context.go('/');
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const Scaffold(
+      body: Center(
+        child: Text('LaunchPage'),
+      ),
+    );
   }
 }
