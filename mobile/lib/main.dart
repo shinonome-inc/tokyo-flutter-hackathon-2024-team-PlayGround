9a812e0d57fdcd17c188dae167cf738c3ee625b2
Yo Kobayashi <82624334+KobayashiYoh@users.noreply.github.com>
2024-10-23 01:00:52 +0900

    main.dartにrouterを追加

--- mobile/lib/main.dart
+++ mobile/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:mobile/config/router.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,22 +10,13 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp.router(
       title: 'Flutter Demo',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(),
+      routerConfig: router,
     );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const Placeholder();
   }
 }
