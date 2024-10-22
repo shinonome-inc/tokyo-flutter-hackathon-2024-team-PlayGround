a094884a1200347c361cef5c7c163d9243c46b32
Yo Kobayashi <82624334+KobayashiYoh@users.noreply.github.com>
2024-10-22 22:37:52 +0900

    仮の画面となるクラスを作成

---
+++ mobile/lib/pages/LaunchPage.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class LaunchPage extends StatelessWidget {
+  const LaunchPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ mobile/lib/pages/MakeoverPage.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class MakeOverPage extends StatelessWidget {
+  const MakeOverPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
