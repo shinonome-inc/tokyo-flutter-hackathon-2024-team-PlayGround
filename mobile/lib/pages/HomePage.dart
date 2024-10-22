49954c3f07aa30ff5ff410120f830f320c782ac0
Yo Kobayashi <82624334+KobayashiYoh@users.noreply.github.com>
2024-10-23 01:17:45 +0900

    HomePageに画面遷移を追加

--- mobile/lib/pages/HomePage.dart
+++ mobile/lib/pages/HomePage.dart
@@ -1,10 +1,44 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text('HomePage'),
+            TextButton(
+              onPressed: () {
+                context.go('/ranking');
+              },
+              child: const Text('ランキング'),
+            ),
+            TextButton(
+              onPressed: () {
+                context.go('/dress-up');
+              },
+              child: const Text('着せ替え'),
+            ),
+            TextButton(
+              onPressed: () {
+                context.go('/makeover');
+              },
+              child: const Text('模様替え'),
+            ),
+            TextButton(
+              onPressed: () {
+                context.go('/setting');
+              },
+              child: const Text('設定'),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
