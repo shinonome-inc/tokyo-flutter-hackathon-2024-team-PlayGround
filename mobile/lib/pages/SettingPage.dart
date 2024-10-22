2d5348b615c1405ace5d33faa9edbc5c460180f2
Yo Kobayashi <82624334+KobayashiYoh@users.noreply.github.com>
2024-10-23 01:19:10 +0900

    SettingPageに画面遷移を追加

--- mobile/lib/pages/SettingPage.dart
+++ mobile/lib/pages/SettingPage.dart
@@ -1,10 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:mobile/repositories/SecureStorageRepository.dart';
 
 class SettingPage extends StatelessWidget {
   const SettingPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('SettingPage'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text('SettingPage'),
+            TextButton(
+              onPressed: () async {
+                await SecureStorageRepository().deleteToken();
+                context.go('/top');
+              },
+              child: const Text('ログアウト'),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
