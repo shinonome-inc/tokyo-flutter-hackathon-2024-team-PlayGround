f8ba6a3b4563f0571b866894430121541fd1af75
Yo Kobayashi <82624334+KobayashiYoh@users.noreply.github.com>
2024-10-26 23:13:48 +0900

    LaunchStateを追加

--- mobile/lib/models/top_state.dart
+++ mobile/lib/models/top_state.dart
@@ -6,11 +6,9 @@ part 'top_state.freezed.dart';
 class TopState with _$TopState {
   const factory TopState({
     required bool isLoading,
-    required String? accessToken,
   }) = _TopState;
 }
 
 const initialTopState = TopState(
   isLoading: false,
-  accessToken: null,
 );
