15657310aea6b4e7c6853b5da65da2d94f73cf43
Yo Kobayashi <82624334+KobayashiYoh@users.noreply.github.com>
2024-10-27 17:18:30 +0900

    LaunchPageで1秒待機する処理を追加

--- mobile/lib/pages/launch_page.dart
+++ mobile/lib/pages/launch_page.dart
@@ -12,18 +12,24 @@ class LaunchPage extends ConsumerStatefulWidget {
 }
 
 class _LaunchPageState extends ConsumerState<LaunchPage> {
+  Future<void> _initialize() async {
+    final notifier = ref.read(launchNotifierProvider.notifier);
+    final isSignedIn = await notifier.isSignedIn();
+    // NOTE: 一瞬の自動画面遷移でユーザーを混乱させないために意図的に1秒待機する。
+    await Future.delayed(const Duration(seconds: 1));
+    if (!mounted) return;
+    if (isSignedIn) {
+      context.go(RouterPaths.home);
+      return;
+    }
+    context.go(RouterPaths.top);
+  }
+
   @override
   void initState() {
     super.initState();
     Future(() async {
-      final notifier = ref.read(launchNotifierProvider.notifier);
-      final isSignedIn = await notifier.isSignedIn();
-      if (!mounted) return;
-      if (isSignedIn) {
-        context.go(RouterPaths.home);
-        return;
-      }
-      context.go(RouterPaths.top);
+      await _initialize();
     });
   }
 
