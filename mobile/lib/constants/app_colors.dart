b4462c67bc0913b8e1c4bfead26f589c4d69d85c
ko_cha78 <kota1725@gmail.com>
2024-10-28 22:53:33 +0900

    AppColorsの定義

---
+++ mobile/lib/constants/app_colors.dart
@@ -0,0 +1,13 @@
+import 'dart:ui';
+
+class AppColors {
+  AppColors._();
+
+  static const dialogBackground = Color(0xFFFAF0C5);
+  static const dialogItemBackground = Color(0xFFFEE167);
+  static const black = Color(0xFF000000);
+  static const darkGray = Color(0xFF333333);
+  static const white = Color(0xFFFFFFFF);
+  static const buttonBackground = Color(0xFFCBAE29);
+  static const scrollBar = Color(0xFFAA9328);
+}
