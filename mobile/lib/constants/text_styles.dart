1e8c0bc2cfad9a5056a760fd8b36a7c17673baa5
ko_cha78 <kota1725@gmail.com>
2024-10-28 22:53:50 +0900

    textstylesの定義

---
+++ mobile/lib/constants/text_styles.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class TextStyles {
+  TextStyles._();
+  static const TextStyle title = TextStyle(
+    fontSize: 24,
+    fontWeight: FontWeight.bold,
+  );
+  static const TextStyle subtitle = TextStyle(
+    fontSize: 14,
+    fontWeight: FontWeight.bold,
+  );
+}
