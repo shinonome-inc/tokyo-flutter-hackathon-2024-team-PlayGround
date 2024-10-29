1b220f35abab981b5ca96380525dcf865821ceef
ko_cha78 <kota1725@gmail.com>
2024-10-30 03:21:26 +0900

    画像pathの追加

--- mobile/lib/constants/image_paths.dart
+++ mobile/lib/constants/image_paths.dart
@@ -4,10 +4,18 @@ class ImagePaths {
 
   static const String defaultUser = 'assets/images/default_user.png';
 
+  static const String backgroundSpring = 'assets/images/background_spring.png';
   static const String backgroundSummer = 'assets/images/background_summer.png';
+  static const String backgroundAutumn = 'assets/images/background_autumn.png';
+  static const String backgroundWinter = 'assets/images/background_winter.png';
 
   static const String dash = 'assets/images/dash.png';
   static const String dashSample1 = 'assets/images/dash_sample1.png';
   static const String dashSample2 = 'assets/images/dash_sample2.png';
   static const String dashSample3 = 'assets/images/dash_sample3.png';
+  static const String dressNormal = 'assets/images/dress_normal.png';
+  static const String dressSpring = 'assets/images/dress_spring.png';
+  static const String dressSummer = 'assets/images/dress_summer.png';
+  static const String dressAutumn = 'assets/images/dress_autumn.png';
+  static const String dressWinter = 'assets/images/dress_winter.png';
 }
