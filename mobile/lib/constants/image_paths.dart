a003046b10687790f2e3da5d483c3cfdbc51eba1
Yo Kobayashi <82624334+KobayashiYoh@users.noreply.github.com>
2024-10-30 23:12:34 +0900

    食事のアニメーションの画像を1枚追加

--- mobile/lib/constants/image_paths.dart
+++ mobile/lib/constants/image_paths.dart
@@ -11,12 +11,20 @@ class ImagePaths {
   static const dashSample2 = 'assets/images/dash_sample2.png';
   static const dashSample3 = 'assets/images/dash_sample3.png';
 
-  static const foodWithEffectStep1 = 'assets/images/food_with_effect_step1.png';
-  static const foodWithEffectStep2 = 'assets/images/food_with_effect_step2.png';
-  static const foodWithEffectStep3 = 'assets/images/food_with_effect_step3.png';
-  static const foodWithEffectStep4 = 'assets/images/food_with_effect_step4.png';
-  static const foodWithEffectStep5 = 'assets/images/food_with_effect_step5.png';
-  static const foodWithEffectStep6 = 'assets/images/food_with_effect_step6.png';
+  static const foodWithEffectStep1 =
+      'assets/images/food_with_effect/food_with_effect_step1.png';
+  static const foodWithEffectStep2 =
+      'assets/images/food_with_effect/food_with_effect_step2.png';
+  static const foodWithEffectStep3 =
+      'assets/images/food_with_effect/food_with_effect_step3.png';
+  static const foodWithEffectStep4 =
+      'assets/images/food_with_effect/food_with_effect_step4.png';
+  static const foodWithEffectStep5 =
+      'assets/images/food_with_effect/food_with_effect_step5.png';
+  static const foodWithEffectStep6 =
+      'assets/images/food_with_effect/food_with_effect_step6.png';
+  static const foodWithEffectStep7 =
+      'assets/images/food_with_effect/food_with_effect_step7.png';
   static const foodWithEffects = [
     foodWithEffectStep1,
     foodWithEffectStep2,
@@ -24,5 +32,6 @@ class ImagePaths {
     foodWithEffectStep4,
     foodWithEffectStep5,
     foodWithEffectStep6,
+    foodWithEffectStep7,
   ];
 }
