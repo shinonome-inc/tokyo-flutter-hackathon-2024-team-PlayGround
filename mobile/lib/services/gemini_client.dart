99a5ccb944a9417ff6c27e812155a05e5d1e70b4
Yo Kobayashi <82624334+KobayashiYoh@users.noreply.github.com>
2024-10-31 13:21:25 +0900

    GeminiClientを追加

---
+++ mobile/lib/services/gemini_client.dart
@@ -0,0 +1,24 @@
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:google_generative_ai/google_generative_ai.dart';
+import 'package:mobile/constants/prompts.dart';
+
+class GeminiClient {
+  GeminiClient._();
+
+  static final GeminiClient instance = GeminiClient._();
+
+  static final _model = GenerativeModel(
+    model: 'gemini-1.5-flash-latest',
+    apiKey: dotenv.env['GEMINI_API_KEY']!,
+  );
+
+  Future<String> postPrompt({required String inputText}) async {
+    final content = [
+      Content.text(Prompts.common),
+      Content.text(inputText),
+    ];
+    final response = await _model.generateContent(content);
+    print(response.text);
+    return response.text ?? '';
+  }
+}
