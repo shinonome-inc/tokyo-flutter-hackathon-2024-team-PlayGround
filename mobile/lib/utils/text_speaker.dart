ccda3e2a852588ced38870b7fa2965034542670e
Yo Kobayashi <82624334+KobayashiYoh@users.noreply.github.com>
2024-10-31 13:25:01 +0900

    path_providerがwebに対応していない影響でWebでbyteを音声ファイルに変換して再生できない不具合を修正

--- mobile/lib/utils/text_speaker.dart
+++ mobile/lib/utils/text_speaker.dart
@@ -1,8 +1,12 @@
+import 'dart:convert';
+
 import 'package:audioplayers/audioplayers.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_tts/flutter_tts.dart';
 import 'package:mobile/services/voice_box_client.dart';
 import 'package:mobile/utils/file_converter.dart';
+import 'package:universal_html/html.dart' as html;
 
 /// テキストの音声読み上げを行う。
 class TextSpeaker {
@@ -27,6 +31,14 @@ class TextSpeaker {
   Future<void> _speakTextWithVoiceBox(String text) async {
     final query = await VoiceBoxClient.instance.createQuery(text: text);
     final bytes = await VoiceBoxClient.instance.createVoice(query: query);
+    if (kIsWeb) {
+      final base64String = base64Encode(bytes);
+      final audio = html.AudioElement()
+        ..src = 'data:audio/wav;base64,$base64String'
+        ..autoplay = true;
+      await audio.play();
+      return;
+    }
     final file = await FileConverter.convertBytesToWavFile(bytes);
     final player = AudioPlayer();
     await player.play(DeviceFileSource(file.path));
