c26382d0119cbc93915629fb0118af166d4b0525
Yo Kobayashi <82624334+KobayashiYoh@users.noreply.github.com>
2024-11-03 00:20:34 +0900

    ApiClientクラスをRepositoriClientに置き換え

--- mobile/lib/pages/auth_callback_page.dart
+++ mobile/lib/pages/auth_callback_page.dart
@@ -1,10 +1,8 @@
-import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:go_router/go_router.dart';
 import 'package:mobile/constants/router_paths.dart';
-import 'package:mobile/repositories/api_client.dart';
 import 'package:mobile/repositories/secure_storage_repository.dart';
+import 'package:mobile/services/repositori_client.dart';
 
 class AuthCallbackPage extends StatefulWidget {
   const AuthCallbackPage({super.key});
@@ -14,13 +12,9 @@ class AuthCallbackPage extends StatefulWidget {
 }
 
 class _AuthCallbackPageState extends State<AuthCallbackPage> {
-  late ApiClient _apiClient;
-
   @override
   void initState() {
     super.initState();
-    final baseUrl = dotenv.env['ENDPOINT']!;
-    _apiClient = ApiClient(Dio(), baseUrl: baseUrl);
     _handleAuthCallback();
   }
 
@@ -38,21 +32,11 @@ class _AuthCallbackPageState extends State<AuthCallbackPage> {
 
   Future<void> _processAuthCode(String code) async {
     try {
-      final response = await _apiClient.getAccessToken(code);
-
-      if (response.response.statusCode == 200) {
-        final accessToken = response.data.access_token;
-
-        // アクセストークンをセキュアストレージに保存
-        await SecureStorageRepository().writeToken(accessToken);
-        print('アクセストークンを保存しました: $accessToken');
-
-        if (mounted) {
-          context.go(RouterPaths.home); // 認証成功時にホーム画面に遷移
-        }
-      } else {
-        print('アクセストークンの取得に失敗しました: ${response.response.statusMessage}');
-        // 必要に応じてエラーメッセージを表示するか、再試行します。
+      final token = await RepositoriClient.instance.fetchAccessToken(code);
+      // アクセストークンをセキュアストレージに保存
+      await SecureStorageRepository().writeToken(token);
+      if (mounted) {
+        context.go(RouterPaths.home); // 認証成功時にホーム画面に遷移
       }
     } catch (e) {
       print('エラーが発生しました: $e');
