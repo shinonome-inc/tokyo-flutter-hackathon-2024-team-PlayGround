be938ed7ea7508df691a846efc23431aaf945631
Yo Kobayashi <82624334+KobayashiYoh@users.noreply.github.com>
2024-11-02 17:27:47 +0900

    GitHubClientを追加

---
+++ mobile/lib/services/git_hub_client.dart
@@ -0,0 +1,31 @@
+import 'package:dio/dio.dart';
+
+class GitHubClient {
+  GitHubClient._() {
+    const baseUrl = 'https://api.github.com/user';
+    _dio.options.baseUrl = baseUrl;
+  }
+
+  final Dio _dio = Dio();
+
+  static final GitHubClient instance = GitHubClient._();
+
+  /// アクセストークンが有効かどうか確認する。
+  Future<bool> isTokenValid(String token) async {
+    _dio.options.headers['Authorization'] = 'token $token';
+    Response<dynamic> response;
+    try {
+      response = await _dio.get('/user');
+    } catch (e) {
+      print(e);
+      return false;
+    } finally {
+      _dio.options.headers.remove('Authorization');
+    }
+    print(response.statusCode);
+    if (response.statusCode == 200) {
+      return true;
+    }
+    return false;
+  }
+}
