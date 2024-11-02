fce5e97d941e0cbe236dcebcd7f42afa45bdf9ae
Yo Kobayashi <82624334+KobayashiYoh@users.noreply.github.com>
2024-11-02 15:57:31 +0900

    LaunchNotifierを追加

---
+++ mobile/lib/providers/launch_notifier.dart
@@ -0,0 +1,32 @@
+import 'package:mobile/models/launch_state.dart';
+import 'package:mobile/repositories/secure_storage_repository.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part 'launch_notifier.g.dart';
+
+@riverpod
+class LaunchNotifier extends _$LaunchNotifier {
+  @override
+  LaunchState build() {
+    return initialLaunchState;
+  }
+
+  void setLoading(bool isLoading) {
+    state = state.copyWith(isLoading: isLoading);
+  }
+
+  void setOpacity(double opacity) {
+    state = state.copyWith(opacity: opacity);
+  }
+
+  Future<bool> isSignedIn() async {
+    final token = await SecureStorageRepository().readToken();
+    // TODO: アクセストークンが有効かどうかGitHubのAPIを叩いて確認する。
+    // TODO: ユーザーを取得する。
+
+    // NOTE: 現在はアクセストークンがローカルに保存されているかどうかで判定している。
+    // NOTE: API通信の待機時間を再現するために擬似的に1.5秒待機している。
+    await Future.delayed(const Duration(milliseconds: 1500));
+    return token != null;
+  }
+}
