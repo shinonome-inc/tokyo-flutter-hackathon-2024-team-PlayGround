a0d0fc4285989b5a56449eb11d75954149f3fc3f
Yo Kobayashi <82624334+KobayashiYoh@users.noreply.github.com>
2024-11-02 11:36:01 +0900

    LevelProgressBarのファイル名のタイポを修正

--- mobile/lib/pages/home_page.dart
+++ mobile/lib/pages/home_page.dart
@@ -4,7 +4,7 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:go_router/go_router.dart';
 import 'package:mobile/components/animated_images_view.dart';
 import 'package:mobile/components/circlar_elevated_button.dart';
-import 'package:mobile/components/level_prograss_bar.dart';
+import 'package:mobile/components/level_progress_bar.dart';
 import 'package:mobile/constants/image_paths.dart';
 import 'package:mobile/constants/router_paths.dart';
 import 'package:mobile/models/dash.dart';
