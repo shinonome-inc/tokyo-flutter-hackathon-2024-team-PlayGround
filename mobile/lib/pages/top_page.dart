4b27ed30a3bee4692f4de470a7ebfa46e75f0845
tatsurou9003 <t.murakami@4nonome.com>
2024-11-02 15:51:38 +0900

    一旦リダイレクトURLをshinonomeに戻す

--- mobile/lib/pages/top_page.dart
+++ mobile/lib/pages/top_page.dart
@@ -17,8 +17,7 @@ class TopPage extends StatefulWidget {
 class _TopPageState extends State<TopPage> {
   final clientId = dotenv.env['GITHUB_CLIENT_ID']!;
   final baseUrl = dotenv.env['ENDPOINT']!;
-  final String redirectUri =
-      'https://d11feo1e8byiei.cloudfront.net/auth-callback';
+  final String redirectUri = 'https://shinonome.com';
   late ApiClient _apiClient;
   late WebViewController _controller;
 
